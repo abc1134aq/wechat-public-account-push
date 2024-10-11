86d6863fb4e6263238513605f2042c84ed3600a9
浪浪 <62324847+abc1134aq@users.noreply.github.com>
2024-10-11 16:15:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,6 +22,7 @@ const USER_CONFIG = {
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o73sO6lwEKW9IvgmsIBwN5VepnE4',
+      id: 'o73sO6pYjhVI11xWbTCefXbF5K-U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'cG22p5oyjHfdOTR7jd_zSF9EWJq6i2yekLL83Ld0wgc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
